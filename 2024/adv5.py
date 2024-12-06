0480adea22c5ddf14cfde1dab8c321e1e9e32fa3
Frederick Nilsen <frederick.nilsen@gmail.com>
2024-12-06 23:31:17 +0100

    Part 2 of day 5 (cursed)

--- 2024/adv5.py
+++ 2024/adv5.py
@@ -16,21 +16,21 @@ def parse_input(filename="input5.txt"):
 
     return ahead_dict, behind_dict, updates
 
+def is_valid_update(update, ahead_dict, behind_dict):
+    for i, num in enumerate(update):
+        ahead_list = ahead_dict.get(num, [])
+        behind_list = behind_dict.get(num, [])
+        ahead_nums = update[:i]
+        behind_nums = update[i+1:]
+        if set(ahead_nums) & set(ahead_list) or set(behind_nums) & set(behind_list):
+            return False
+    return True
+
+
 def get_valid_updates(updates, ahead_dict, behind_dict):
     valid_updates = []
     for update in updates:
-        current_valid = True
-        for i, num in enumerate(update):
-            ahead_list = ahead_dict.get(num, [])
-            behind_list = behind_dict.get(num, [])
-            ahead_nums = update[:i]
-            behind_nums = update[i+1:]
-            # If any numbers ahead are in behind list, the update is invalid
-            # If any numbers behind are in the ahead list, the update is invalid
-            if set(ahead_nums) & set(ahead_list) or set(behind_nums) & set(behind_list):
-                current_valid = False
-                break
-        if current_valid:
+        if is_valid_update(update, ahead_dict, behind_dict):
             valid_updates.append(update)
     return valid_updates
 
@@ -41,7 +41,28 @@ def part_1(ahead_dict, behind_dict, updates):
 def part_2(ahead_dict, behind_dict, updates):
     valid_updates = get_valid_updates(updates, ahead_dict, behind_dict)
     invalid_updates = [u for u in updates if u not in valid_updates]
-    print(invalid_updates)
+    fixed_updates = []
+    for update in invalid_updates:
+        while not is_valid_update(update, ahead_dict, behind_dict):
+            for i, num in enumerate(update):
+                ahead_list = ahead_dict.get(num, [])
+                behind_list = behind_dict.get(num, [])
+                ahead_nums = update[:i]
+                behind_nums = update[i + 1 :]
+                should_be_after = set(ahead_nums) & set(ahead_list)
+                should_be_before = set(behind_nums) & set(behind_list)
+                if should_be_before:
+                    update[i], update[i + len(should_be_before)] = (
+                        update[i + len(should_be_before)],
+                        update[i],
+                    )
+                elif should_be_after:
+                    update[i], update[i - len(should_be_after)] = (
+                        update[i - len(should_be_after)],
+                        update[i],
+                    )
+        fixed_updates.append(update)
+    return sum(vu[len(vu) // 2] for vu in fixed_updates)
 
 
 if __name__ == '__main__':
